3ff47791d79f1ce32583e34fea63bad9dd1168ef
Devisetti Upendra <NH2042@nslhub.in>
2023-10-13 16:55:59 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Demo extends StatefulWidget {
+  const Demo({super.key});
+
+  @override
+  State<Demo> createState() => _DemoState();
+}
+
+class _DemoState extends State<Demo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
